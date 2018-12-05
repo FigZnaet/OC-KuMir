5ede7e04eb658739841ccc37b6b28a53797c0c7a
VladG24 YT <vg07102004@gmail.com>
2018-12-05 20:31:20 +0300

    Update kumir-classic.lua

--- src/OpenOS/KuMir/bin/kumir-classic.lua
+++ src/OpenOS/KuMir/bin/kumir-classic.lua
@@ -1,1 +1,14 @@
+local component = require("component")
+local fs = require("filesystem")
+local gpu = component.gpu
+local w,h = gpu.getResolution()
+
+gpu.setResolution(16, 16);
+gpu.setBackground(0x000099);
+gpu.setForeground(0xFFFF00);
+gpu.fill(0, 0, 16, 16, " ");
+gpu.set(4, 3, "00  00");
+gpu.set(4, 4, "00000");
+gpu.set(4, 5, "000000");
+gpu.set(4, 6, "00  00");
 --!КуМир-Стандарт
