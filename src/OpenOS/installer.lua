fb300466052d72530da9f8535d13686a163f4802
VladG24 YT <vg07102004@gmail.com>
2018-12-05 19:37:52 +0300

    Update installer.lua

--- src/OpenOS/installer.lua
+++ src/OpenOS/installer.lua
@@ -8,37 +8,29 @@ gpu.setForeground(0xFFFF33);
 
 term.write("\n\nЗапускаю установщик КуМир...\n\n");
 
-local url = "https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS/";
+local url = "https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS";
+local files = {
+  kumir = "/bin/kumir.lua",
+  config = "/KuMir/version.cfg",
+  defaultLib = "/lib/KDefault.lua",
+  robotLib = "/lib/KRobot.lua",
+  waterLib = "/lib/KWater.lua",
+  drawerLib = "/lib/KDrawer.lua",
+  painterLib = "/lib/KPainter.lua",
+  turnerLib = "/lib/KTurner.lua",
+}
+local tutorials = {
+  firstLaunch = "/KuMir/tutorials/FirstLaunch.txt" 
+}
+local examples = {
+  fsK = "/KuMir/examples/standard/FirstEx.kum",
+  fsF = "/KuMir/examples/standard/FirstEx.fl",
+  --[Robot]
+  frK = "/KuMir/examples/robot/FirstEx.kum",
+  frF = "/KuMir/examples/robot/FirstEx.fl"
+}
 
---Bin
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS/bin/kumir.lua -> /bin/kumir.lua\n");
-loadfile("/bin/wget.lua")(url.."bin/kumir.lua", "/bin/kumir.lua", "-f");
-
---Ver
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS/KuMir/version.cfg -> /bin/KuMir/version.cfg\n");
-loadfile("/bin/wget.lua")(url.."KuMir/version.cfg", "/KuMir/version.cfg", "-f");
-
---Lib
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS/KuMir/version.cfg -> /bin/KuMir/lib/Robot.lua\n");
-loadfile("/bin/wget.lua")(url.."KuMir/lib/Robot.lua", "/KuMir/Robot.lua", "-f");
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS/KuMir/version.cfg -> /bin/KuMir/lib/Water.lua\n");
-loadfile("/bin/wget.lua")(url.."KuMir/lib/Water.lua", "/KuMir/lib/Water.lua", "-f");
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS/KuMir/version.cfg -> /bin/KuMir/lib/Drawer.lua\n");
-loadfile("/bin/wget.lua")(url.."KuMir/lib/Drawer.lua", "/KuMir/lib/Drawer.lua", "-f");
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS/KuMir/version.cfg -> /bin/KuMir/lib/Painter.lua\n");
-loadfile("/bin/wget.lua")(url.."KuMir/lib/Painter.lua", "/KuMir/lib/Painter.lua", "-f");
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/src/OpenOS/KuMir/lib/Turner.lua -> /bin/KuMir/lib/Turner.lua\n");
-loadfile("/bin/wget.lua")(url.."KuMir/lib/Turner.lua", "/KuMir/lib/Turner.lua", "-f");
-
---Tutorials
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/KuMir/Tutorials/FirstLaunch.txt -> /bin/KuMir/Tutorials/FirstLaunch.txt\n");
-loadfile("/bin/wget.lua")(url.."KuMir/tutorials/FirstLaunch.txt", "/KuMir/tutorials/FirstLaunch.txt", "-f");
-
---Examples Basic
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/KuMir/examples/basic/Base.kum -> /bin/KuMir/Examples/Basic/Base.kum\n");
-loadfile("/bin/wget.lua")(url.."KuMir/examples/basic/Base.kum", "/KuMir/Examples/Basic/Base.kum", "-f");
---Examples Robot
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/KuMir/examples/robot/RobotBase.kum. -> /bin/KuMir/Examples/Robot/RobotBase.kum\n");
-loadfile("/bin/wget.lua")(url.."KuMir/examples/robot/RobotBase.kum", "/KuMir/Examples/Robot/RobotBase.kum", "-f");
-term.write("https://raw.githubusercontent.com/Vladg24YT/OC-KuMir/master/KuMir/examples/robot/RobotBase.fil. -> /bin/KuMir/Examples/Robot/RobotBase.fil\n");
-loadfile("/bin/wget.lua")(url.."KuMir/examples/robot/RobotBase.fil", "/KuMir/Examples/Robot/RobotBase.fil", "-f");
+for i, value in ipairs(files) do
+  term.write(url..value.." -> "..value);
+  loadfile("/bin/wget.lua")(url..value,value,"-f");
+end
