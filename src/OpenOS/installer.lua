f135d78fdd8bf246e53f96a7f3f81b6f6fcf5e64
VladG24 YT <vg07102004@gmail.com>
2018-12-05 19:59:21 +0300

    Update installer.lua

--- src/OpenOS/installer.lua
+++ src/OpenOS/installer.lua
@@ -17,18 +17,18 @@ local files = {
   waterLib = "/lib/KWater.lua",
   drawerLib = "/lib/KDrawer.lua",
   painterLib = "/lib/KPainter.lua",
-  turnerLib = "/lib/KTurner.lua"
-};
+  turnerLib = "/lib/KTurner.lua",
+}
 local tutorials = {
-  firstLaunch = "/KuMir/tutorials/FirstLaunch.txt" 
-};
+  firstLaunch = "/KuMir/tutorials/FirstLaunch.txt",
+}
 local examples = {
   fsK = "/KuMir/examples/standard/FirstEx.kum",
   fsF = "/KuMir/examples/standard/FirstEx.fl",
   --[Robot]
   frK = "/KuMir/examples/robot/FirstEx.kum",
-  frF = "/KuMir/examples/robot/FirstEx.fl"
-};
+  frF = "/KuMir/examples/robot/FirstEx.fl",
+}
 
 term.write("\n\nУстанавливаю Систему программирования КуМир...\n\n");
 for key, value in pairs(files) do
