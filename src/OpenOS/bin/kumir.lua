67df4cc1e6479d61f9bb9750bbd5c0c884c57cc0
VladG24 YT <vg07102004@gmail.com>
2018-12-07 16:17:24 +0300

    Update kumir.lua

--- src/OpenOS/bin/kumir.lua
+++ src/OpenOS/bin/kumir.lua
@@ -38,13 +38,13 @@ term.write("\n0.) Выход\n\n");
 
 local i = term.read();
 if i==1 then
-  loadfile("/./KuMir/bin/kumir-classic.lua");
+  loadfile("/kumir/kumir-classic.lua");
 elseif i==2 then
-  loadfile("/./KuMir/bin/kumir-highgrade.lua");
+  loadfile("/kumir/kumir-highgrade.lua");
 elseif i==3 then
-  loadfile("/./KuMir/bin/kumir-ide.lua");
+  loadfile("/kumir/kumir-ide.lua");
 elseif i==4 then
-  loadfile("/./KuMir/bin/kumir-teacher.lua");
+  loadfile("/kumir/kumir-teacher.lua");
 else
   return
 end
